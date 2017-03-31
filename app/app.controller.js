fa1be41fb89fbcc64b8155c78bae8635c82553f5
Karthik Bala <karthik@appnlogic.com>
2017-03-31 16:18:03 +0530

    added the global api server path

--- app/app.controller.js
+++ app/app.controller.js
@@ -3,9 +3,21 @@
  *  controller
  */
 
+// angular
+    // .module('altairApp')
+    // .controller('mainCtrl', [
+        // function () {}
+    // ])
+// ;
+
 angular
     .module('altairApp')
-    .controller('mainCtrl', [
-        function () {}
-    ])
-;
+    .controller('mainCtrl',
+        function($scope, $http, $rootScope, $localStorage,$location) {
+			$localStorage.service='http://192.168.1.136/rubyServices/api/';
+            //alert($localStorage.access_token);
+			if($localStorage.access_token=='' || $localStorage.access_token==undefined){
+				$location.path('/');
+			}
+        }
+    );
