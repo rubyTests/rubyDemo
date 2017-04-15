90fb9ddb7c7074cc9182393fca0b30a65c0d6cfd
Karthik Bala <karthik@appnlogic.com>
2017-04-15 19:40:52 +0530

    added the upload url service

--- app/app.controller.js
+++ app/app.controller.js
@@ -3,6 +3,7 @@ angular
     .controller('mainCtrl',
         function($scope, $http, $rootScope, $localStorage,$location) {
 			$localStorage.service='http://192.168.1.136/rubyServices/api/';
+			$localStorage.uploadUrl='http://192.168.1.136/rubyServices/upload/';
             //alert($localStorage.access_token);
 			if($localStorage.access_token=='' || $localStorage.access_token==undefined){
 				$location.path('/');
