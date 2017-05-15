7c2bc6082f9d7c1ed24b6f5e1297b19cff5a0c7a
Karthik Bala <karthik@appnlogic.com>
2017-05-15 11:47:26 +0530

    completed the vacate module

--- app/components/hostel/vacateCtrl.js
+++ app/components/hostel/vacateCtrl.js
@@ -1,8 +1,10 @@
 angular
     .module('rubycampusApp')
     .controller('vacateCtrl',
-        function($compile, $scope, $timeout, $resource, DTOptionsBuilder, DTColumnDefBuilder,$filter) {
-            var vm = this;
+        function($compile, $scope, $timeout, $resource, DTOptionsBuilder, DTColumnDefBuilder,$filter,$http,$localStorage) {
+            $scope.btnStatus="Save";
+			$scope.vacate={id:'',selectize_usertype:'',selectize_employee:'',selectize_student:'',vacate_date:''};
+			var vm = this;
             $scope.viewData=[];
             vm.dtOptions = DTOptionsBuilder
                 .newOptions()
@@ -69,41 +71,124 @@ angular
                     placeholder: 'Select Resident'
                 };
 
-                $scope.selectize_employee_options = ['Vijay Raj','Karthik Selvam','Mani Vannan','Senthil Kumar','Mani Kandan','Junaid Muhammed'];
+               
+				$scope.empName =[];
+                $http.get($localStorage.service+'HostelAPI/allocateEmployeeDetail',{headers:{'access_token':$localStorage.access_token}})
+                .success(function(user_data){
+                    $scope.empName.push(user_data.data);
+                });
+                $scope.selectize_employee_options =$scope.empName;
                 $scope.selectize_employee_config = {
                     create: false,
                     maxItems: 1,
-                    placeholder: 'Select Employee'
+                    placeholder: 'Employee',
+                    valueField: 'ID',
+                    labelField: 'NAME',
+                    searchField: 'NAME',
+                    onInitialize: function(selectize){
+                        selectize.on('change', function(value) {
+                        });
+                    }
                 };
-                $scope.selectize_student_options = ['Vijay Raj','Karthik Selvam','Mani Vannan','Senthil Kumar','Mani Kandan','Junaid Muhammed'];
+                $scope.studentName =[];
+                $http.get($localStorage.service+'HostelAPI/allocateStudentDetail',{headers:{'access_token':$localStorage.access_token}})
+                .success(function(user_data){
+                    $scope.studentName.push(user_data.result);
+                });
+                $scope.selectize_student_options =$scope.studentName;
                 $scope.selectize_student_config = {
                     create: false,
                     maxItems: 1,
-                    placeholder: 'Select Student'
+                    placeholder: 'Student',
+                    valueField: 'ID',
+                    labelField: 'NAME',
+                    searchField: 'NAME',
+                    onInitialize: function(selectize){
+                        selectize.on('change', function(value) {
+                        });
+                    }
                 };
 
-                 $resource('app/components/hostel/alloacation.json')
-                .query()
-                .$promise
-                .then(function(allac_data) {
-                    $scope.viewData=allac_data;
-                });
+                 // $resource('app/components/hostel/alloacation.json')
+                // .query()
+                // .$promise
+                // .then(function(allac_data) {
+                    // $scope.viewData=allac_data;
+                // });
+				
+				// Table View 
+				$http({
+				method:'get',
+				url: $localStorage.service+'HostelAPI/vacate',
+				headers:{'access_token':$localStorage.access_token}
+				}).then(function(return_data){
+					console.log(return_data.data.message,"msg");
+					$scope.viewData=return_data.data.message;
+				});
+				
+				$scope.refreshTable=function(){
+					$http({
+					method:'get',
+					url: $localStorage.service+'HostelAPI/vacate',
+					headers:{'access_token':$localStorage.access_token}
+					}).then(function(return_data){
+						console.log(return_data.data.message,"msg");
+						$scope.viewData=return_data.data.message;
+					});
+				}
 
-                $scope.addAllocation=function(){
-                    $scope.btnStatus="Save";
-                    $scope.selectize_usertype='';
-                    $scope.selectize_employee='';
-                    $scope.selectize_student='';
-                    $scope.vacate_date='';
-                    $('.uk-modal').find('input').trigger('blur');
+                $scope.addVacate=function(){
+                    // $scope.selectize_usertype='';
+                    // $scope.selectize_employee='';
+                    // $scope.selectize_student='';
+                    // $scope.vacate_date='';
+					//console.log($scope.vacate);
+					
+					if($scope.vacate.selectize_usertype=='Student'){
+						$scope.profileId=$scope.vacate.selectize_student
+					}else{
+						$scope.profileId=$scope.vacate.selectize_employee
+					}
+					
+					$http({
+                    method:'POST',
+                    url: $localStorage.service+'HostelAPI/vacate',
+                    data: {
+                        'id' : $scope.vacate.id,
+                        'type' : $scope.vacate.selectize_usertype,
+                        'profileId' : $scope.profileId,
+                        'date' : $scope.vacate.vacate_date
+                    },
+                    headers:{'access_token':$localStorage.access_token}
+                    }).then(function(return_data){
+                        if(return_data.data.status==true){
+                            UIkit.modal("#modal_overflow").hide();
+                            UIkit.notify({
+                                message : return_data.data.message,
+                                status  : 'success',
+                                timeout : 2000,
+                                pos     : 'top-center'
+                            });
+                            $scope.refreshTable();
+                        }
+                    });
+					
+                    //$('.uk-modal').find('input').trigger('blur');
                 }
-                $scope.editAllocation=function(data){
+                $scope.editVacate=function(data){
                     $scope.btnStatus="Update";
                     if (data) {
-                        $scope.selectize_usertype=data.type;
-                        $scope.selectize_employee=data.name;
-                        $scope.selectize_student=data.name;
-                        $scope.vacate_date=data.reg_date;
+						if(data.RESIDENT_TYPE=='Student'){
+							$scope.vacate={id:data.ID,selectize_usertype:data.RESIDENT_TYPE,selectize_employee:'',selectize_student:data.PROFILE_ID,vacate_date:data.DATE};
+						}else{
+							$scope.vacate={id:data.ID,selectize_usertype:data.RESIDENT_TYPE,selectize_employee:data.PROFILE_ID,selectize_student:'',vacate_date:data.DATE};
+						}
+						//$scope.vacate={id:'',selectize_usertype:'',selectize_employee:'',selectize_student:'',vacate_date:data.DATE};
+						//$scope.getId=data.ID;
+                        // $scope.selectize_usertype=data.type;
+                        // $scope.selectize_employee=data.name;
+                        // $scope.selectize_student=data.name;
+                        // $scope.vacate_date=data.reg_date;
                     }
                 }
         }
