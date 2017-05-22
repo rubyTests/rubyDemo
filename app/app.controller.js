5ea9aada45958bf9282507adfa315b8633f6f6d0
Karthik Bala <karthik@appnlogic.com>
2017-05-22 10:15:47 +0530

    added server path

--- app/app.controller.js
+++ app/app.controller.js
@@ -4,6 +4,8 @@ angular
         function($scope, $http, $rootScope, $localStorage,$location) {
 			$localStorage.service='http://192.168.1.139/rubyServices/api/';
 			$localStorage.uploadUrl='http://192.168.1.139/rubyServices/upload/';
+			// $localStorage.service='http://campusenter.com/services/api/';
+			// $localStorage.uploadUrl='http://campusenter.com/services/upload/';
             //alert($localStorage.access_token);
 			if($localStorage.access_token=='' || $localStorage.access_token==undefined){
 				$location.path('/');
