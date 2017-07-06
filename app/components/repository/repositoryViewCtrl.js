d8228a75a42edd7d8bde9f451166c066123e5aab
senthil-appnlogic <sentilkumar@appnlogic.com>
2017-07-06 19:11:18 +0530

    Repository Updated

--- app/components/repository/repositoryViewCtrl.js
+++ app/components/repository/repositoryViewCtrl.js
@@ -7,7 +7,8 @@ angular
         'utils',
         'repository_articles',
         '$localStorage',
-        function ($stateParams,$scope,$rootScope,utils,repository_articles,$localStorage) {
+        '$http',
+        function ($stateParams,$scope,$rootScope,utils,repository_articles,$localStorage,$http) {
             $rootScope.toBarActive = true;
             $scope.$on('$destroy', function() {
                 $rootScope.toBarActive = false;
@@ -21,6 +22,16 @@ angular
 			
             $scope.repository_articles = repository_articles;
 
+            $scope.viewData=[];
+            $http({
+                method:'GET',
+                url: $localStorage.service+'RepositoryAPI/Rep_Post',
+                headers:{'access_token':$localStorage.access_token}
+            }).then(function(return_data){
+                $scope.viewData=return_data.data.message;
+                console.log($scope.viewData,'$scope.viewData');
+            });
+
             $scope.article = utils.findByItemId($scope.repository_articles, $stateParams.articleId);
 
             $scope.getYTSrc = function(src) {
@@ -31,14 +42,38 @@ angular
                 return 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + src + '&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true';
             };
 
-            var categoryData = $scope.repository_articles;
-            var categoryDataNames = categoryData.map(function(item){return item.Repository_Category;});
+            // var categoryData = $scope.repository_articles;
+            // var categoryDataNames = categoryData.map(function(item){return item.Repository_Category;});
 
-            $scope.selectize_category_options = categoryDataNames;
+            // $scope.selectize_category_options = categoryDataNames;
+            // $scope.selectize_category_config = {
+            //     create: false,
+            //     maxItems: 1,
+            //     placeholder: 'Find Catecory'
+            // };
+
+            $scope.CategoryData=[];
+            $http({
+                method : 'GET',
+                url : $localStorage.service+'RepositoryAPI/Rep_Category',
+                headers:{'access_token':$localStorage.access_token}
+            }).then(function(return_data){
+                $scope.CategoryData.push(return_data.data.message);
+            })
+
+            $scope.selectize_category_options =$scope.CategoryData;
             $scope.selectize_category_config = {
                 create: false,
                 maxItems: 1,
-                placeholder: 'Find Catecory'
+                placeholder: 'Select Category',
+                valueField: 'ID',
+                labelField: 'NAME',
+                searchField: 'NAME',
+                onInitialize: function(selectize){
+                    selectize.on('change', function(value) {
+                        console.log(value);
+                    });
+                }
             };
 
         }
